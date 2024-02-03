cb827534c362fae973b41f20b28537e541e7e3ea
Andy Brenneke <andy.brenneke@ironcladhq.com>
2024-02-03 15:45:36 -0800

    wip app

---
+++ app/src/App.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import ChessBoard from './lib/ChessBoard.svelte';
+</script>
+
+<main class="container">
+  <div class="board">
+    <ChessBoard />
+  </div>
+</main>
+
+<style>
+  main {
+    width: 100%;
+    height: 100%;
+
+    background-color: #e0e5ec;
+
+    display: grid;
+    grid-template-columns: 1fr 300px;
+  }
+</style>
---
+++ app/src/lib/ChessBoard.svelte
@@ -0,0 +1,34 @@
+<script>
+  let size = 8;
+  let board = Array.from({ length: size }, (_, i) => Array.from({ length: size }, (_, j) => ((i + j) % 2 === 0 ? 'white' : 'black')));
+</script>
+
+<div class="board">
+  {#each board as row, i}
+    {#each row as cell, j}
+      <div class="cell {cell}" id="{i}-{j}"></div>
+    {/each}
+  {/each}
+</div>
+
+<style>
+  .board {
+    display: grid;
+    grid-template-columns: repeat(8, 1fr);
+    grid-template-rows: repeat(8, 1fr);
+    width: 100vh;
+    max-width: 100%;
+    max-height: 100%;
+
+    aspect-ratio: 1 / 1;
+  }
+  .cell {
+    border: 1px solid black;
+  }
+  .white {
+    background: #f0d9b5;
+  }
+  .black {
+    background: #b58863;
+  }
+</style>
